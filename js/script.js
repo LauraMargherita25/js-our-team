74166aae197d51a6eb578256ab1cf59a5224b3a0
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-07 16:30:48 +0100

    Trying to stamp a single card

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
+/* ---CONSEGNA--- */
+
 /* Viene fornito un layout di base. Come prima cosa nel file js definitevi un array di oggetti che rappresentano i membro del team (prendete i dati dallo screen fornito). Ogni membro deve avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
 Prendendo come riferimento la card di esempio presente nell'html, stampiamo dinamicamente una card per ogni membro del team. */
 
 
+/* ---RECAP & BRAINSTORMING--- */
+
 /* mi conviene forse usare un array di oggetti */
 /* e poi usare un ciclo while per stampare tutti gli oggetti */
 
-const arrTeamMembers = [
+/* const arrTeamMembers = [
     {
     name: 'un nome',
     role: 'un lavoro'
     }
-]
+] */
 
 // console.log(teamMembers.name);    // per stampare il valore di name con il punto
 // console.log(teamMembers['name']); // per stampare il valore di name con le[]
@@ -24,3 +28,18 @@ const arrTeamMembers = [
 
 // per stampare  il valore di un oggetto dentro un array
 // console.log(arrTeamMembers[0].name)
+
+
+/* ---SVOLGIMENTO--- */
+
+// controllo se riesco ad appendere solo una card
+const eleTeamContainer = document.querySelector('team-container');
+const eleTeamCard = document.createElement("div");
+eleTeamCard.classList.add('team-card');
+eleTeamCard.innerHTML = //papina tra backtick;
+eleTeamContainer.append(eleTeamCard);
+
+
+const eleCardImg = document.querySelector('card-image');
+const eleCardText = document.querySelector('card-text');
+
