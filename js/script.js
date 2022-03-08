3127cf293f64bbc655c5caf09215ce11c95364d6
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-08 09:15:57 +0100

    fix puntini

--- js/script.js
+++ js/script.js
@@ -42,37 +42,37 @@ const arrTeamMembers = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        img: '../img/wayne-barnett-founder-ceo.jpg'
+        img: 'img/wayne-barnett-founder-ceo.jpg'
     },
     
     {
         name: 'Angela Carrol',
         role: 'Chief Editor',
-        img: '../img/angela-caroll-chief-editor.jpg'
+        img: 'img/angela-caroll-chief-editor.jpg'
     },
     
     {
         name: 'Walter Gordon',
-    role: 'Office Manager',
-    img: '../img/walter-gordon-office-manager.jpg'
+        role: 'Office Manager',
+        img: 'img/walter-gordon-office-manager.jpg'
     },
 
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        img: '../img/angela-lopez-social-media-manager.jpg'
+        img: 'img/angela-lopez-social-media-manager.jpg'
     },
 
     {
         name: 'Scott Estrada',
         role: 'Developer',
-        img: '../img/scott-estrada-developer.jpg'
+        img: 'img/scott-estrada-developer.jpg'
     },
 
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        img: '../img/barbara-ramos-graphic-designer.jpg'
+        img: 'img/barbara-ramos-graphic-designer.jpg'
     },
 
 ];
